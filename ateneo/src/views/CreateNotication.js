78926f9d4ae297f65cca4913e6009b935776232d
mpolox <mpolox@gmail.com>
2018-11-19 19:23:35 -0600

    select

--- ateneo/src/views/CreateNotication.js
+++ ateneo/src/views/CreateNotication.js
@@ -1,50 +1,61 @@
 import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core';
+// import CardActionArea from '@material-ui/core/CardActionArea';
+// import CardContent from '@material-ui/core/CardContent';
+// import CardMedia from '@material-ui/core/CardMedia';
+// import Typography from '@material-ui/core/Typography';
+import { withStyles, TextField, InputLabel, Select, MenuItem, Grid } from '@material-ui/core';
 
 
 const classes = {
-    card: {
-        maxWidth: 505,
-        border: 10,
-        margin: 14
-    },
-    media: {
-      height: 80,
-    },
-  };
+  card: {
+    maxWidth: 1110,
+    border: 10,
+    margin: 14
+  },
+  media: {
+    height: 80,
+  },
+  text: {
+    width: 250
+  }
+};
 
 class CreateNotication extends Component {
-    
-    render() {
 
-        const {classes} = this.props;
+  render() {
 
-        return (
-            <Card className={ classes.card }>
-            <CardActionArea>
-              <CardMedia
-                className= { classes.media }
-                image="https://quick-catch.com/wp-content/uploads/2017/09/lizard-control-removal.png"
-                title="Contemplative Reptile"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Lizard
-                </Typography>
-                <Typography component="p">
-                  Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                  across all continents except Antarctica
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        );
-    }
+    const { classes } = this.props;
+
+    return (
+      <Card className={classes.card}>
+        <TextField
+          id="outlined-with-placeholder"
+          label="Escriba la notificación"
+          className={classes.textField}
+          margin="normal"
+          multiline
+          rows="6"
+          variant="outlined"
+          fullWidth
+        />
+
+        <Grid item xs={4}>
+        <InputLabel  className={classes.text}>Enviar mensaje por:</InputLabel>
+        <Select>
+          <MenuItem value={1}>Calificación</MenuItem>
+          <MenuItem value={2}>Alumno</MenuItem>
+          <MenuItem value={3}>Profesor</MenuItem>
+          <MenuItem value={4}>Materia</MenuItem>
+          <MenuItem value={4}>Semestre</MenuItem>
+        </Select>
+        </Grid>
+          <br></br>
+
+
+      </Card>
+    );
+  }
 }
 
 export default withStyles(classes)(CreateNotication);
